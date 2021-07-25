e103fda1bd427ff21f2d86d31f797e20bacbccdb
baoaolei1989 <baoaolei1989@outlook.com>
2021-07-25 23:08:33 +0800

    haystack v0.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,17 @@
+import os
+
 import requests
 import streamlit as st
 import streamlit.components.v1 as components
+import altair as alt
+import pandas as pd
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # side bar
-AI_FUNCTIONALITY = ["Question and Answering", "Neural Search"]
+AI_FUNCTIONALITY = ["Neural Search"]
 st.sidebar.subheader("AI Functionality")
 ai_function = st.sidebar.radio("", AI_FUNCTIONALITY)
 
@@ -11,71 +19,79 @@ st.sidebar.subheader("Retriever")
 RETRIEVERS = ["DPR", "BM25"]
 retriever = st.sidebar.radio("", RETRIEVERS)
 
-st.sidebar.subheader("Searching Parameter")
-RE_RETRIEVER_RANGE = list(range(20, 520, 20))
-top_k_retriever = st.sidebar.select_slider("Top K Retriever", options=RE_RETRIEVER_RANGE)
 
-RANKER_RANGE = list(range(5, 105, 5))
-top_k_ranker = st.sidebar.select_slider("Top K Ranker", options=RANKER_RANGE)
 
-FILTERS = ["All", "Title", "Abstract Description", "content"]
-filters = st.sidebar.selectbox("Filters", FILTERS)
 
 # main page
 query = st.text_input("Query")
 
 if query:
     query_data = {
-        "top_k_retriever": top_k_retriever,
-        "top_k_ranker": top_k_ranker,
         "retriever": retriever,
-        "filters": filters,
-        "re-rank": "yes",
         "data": [query]
     }
     if ai_function == AI_FUNCTIONALITY[0]:
-        # question and answering
-        URL = "http://localhost:5000/api/qa"
-        result = requests.post(URL, json=query_data).json()
-        if result["Status"] == 0:
-            answers = result["data"]
-            for answer in answers:
-                html = """ 
-                <div class="card" style="color: white;">
-                    <div>
-                        <b>Title</b>: {title}<br>
-                        <b>score</b>: {score}
-                    </div>
-                    <p>
-                    {content_prev}
-                    <span style="font-weight: bold; background-color: deepskyblue;padding: 4px;">{answer}<span style="color: lightgreen">&nbsp&nbspAnswer </span></span>
-                    {content_after}
-                    </p>
-                </div>
-                """.format(content_prev=answer["context"][:answer["offset_start"]],
-                           answer=answer["context"][answer["offset_start"]:answer["offset_end"]],
-                           content_after=answer["context"][answer["offset_end"]:],
-                           title=answer["title"], score=round(answer["score"],3)
-                           )
-                components.html(html)
-                st.markdown("***")
-
-    else:
-        URL = "http://localhost:5000/api/search"
+        URL = os.environ["SERVER_ENDPOINT"]
         result = requests.post(URL, json=query_data).json()
         if result["Status"] == 0:
             documents = result["data"]
-            for doc in documents:
-                with st.beta_container():
-                  md = """ 
-                        **Title**: {title}  
-                        **score**: {score}  
-                        **type**: {type}  
-                        """.format(
-                            type=doc["type"],
-                            title=doc["title"],
-                            score=round(doc["score"],3),
-                        )
-                  st.markdown(md)
-                  st.write(doc["text"])
-                st.markdown("***")
+            df = pd.DataFrame(documents)
+            df["file_id"] = df["file_id"].astype(int)
+
+            st.subheader("Detail data")
+            st.write(df[["file_id", "sentence", "score", "type"]])
+
+            st.subheader("Analysis")
+            st.markdown("**Top K Documents**")
+            group_df = df.groupby("file_id")["score"].sum().reset_index().sort_values("score", ascending=False)
+
+            c = alt.Chart(group_df).mark_bar().encode(
+                x = alt.X("file_id:N", sort="-y"),
+                y = alt.Y("score:Q"),
+                color = alt.Color("score:Q")
+            ).transform_window(
+                rank='rank(score)',
+                sort=[alt.SortField('score', order='descending')]
+            ).transform_filter(
+                (alt.datum.rank < 20)
+            )
+            st.altair_chart(c, use_container_width=True)
+
+            selected_file_id = st.selectbox("Select file_id to Analyze Sentence", options=group_df["file_id"].tolist())
+            if selected_file_id:
+                rows = df[df.file_id == selected_file_id].iloc[0]["raw_sentences"]
+                sentences_df = pd.DataFrame(rows, columns=["sentence", "type", "file_id"])
+                sentences_df["file_id"] = sentences_df["file_id"].astype(int)
+                sentences_df = sentences_df.merge(df[["sentence","score","file_id"]], how="left", on=["file_id","sentence"]).drop_duplicates()
+                sentences_df["score"].fillna(0, inplace=True)
+                st.write(sentences_df)
+
+                in_abs = False
+                in_content = False
+                group_by_type = sentences_df.groupby("type")
+
+                d = {}
+                for _type, group in group_by_type:
+                    if _type not in d:
+                        d[_type] = []
+                    for _, row in group.iterrows():
+                        text = row["sentence"].strip("\n")
+                        if 0 < row["score"] < 0.2:
+                            text = f"<span class='card score1' style='color: white; background-color: #F4F269; padding: 4px;'>{text}</span>"
+                        elif 0.2 <= row["score"] < 0.4:
+                            text = f"<span class='card score2'  style='color: white; background-color: #CEE26B; padding: 4px;'>{text}</span>"
+                        elif 0.4 <= row["score"] < 0.6:
+                            text = f"<span class='card score3' style='color: white; background-color: #A8D26D; padding: 4px;'>{text}</span>"
+                        elif 0.6 <= row["score"] < 0.8:
+                            text = f"<span class='card score4' style='color: white; background-color: #82C26E; padding: 4px;'>{text}</span>"
+                        elif 0.8 <= row["score"]:
+                            text = f"<span class='card score5' style='color: white; background-color: #5Cb270; padding: 4px;'>{text}</span>"
+                        else:
+                            text = f"<span class='card' style='color: white;  padding: 4px;'>{text}</span>"
+                        d[_type].append(text)
+                st.subheader('Title')
+                components.html("".join(d["Title"]))
+                st.subheader('Abstract Description')
+                components.html("<br/>".join(d["Abstract Description"]), scrolling=True)
+                st.subheader('Content')
+                components.html("<br/>".join(d["Content"]), scrolling=True, height=300)
